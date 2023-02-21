27766017fa117a40ddf1ceeb1927e5d5f9f5721c
Dina-Ursula <dinatyan26@gmail.com>
2023-02-21 12:56:22 +0200

    add/install-emotion-styled

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,19 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
     </div>
   );
 };
+export default App;
+
+// import styled from '@emotion/styled';
+// import {
+//   FriendDescr,
+//   Status,
+//   ImgAvatar,
+//   NameFriend,
+// } from './FriendList.styled';
